4a303fea06f393a28284c61ac2c0ab2f868167d5
mailen niripil <niripil.mailen@gmail.com>
2024-10-31 17:23:44 -0300

    ultima modificacion,clase materia

--- ResultadosEstudiantes/Clases/Estudiante.cs
+++ ResultadosEstudiantes/Clases/Estudiante.cs
@@ -8,7 +8,7 @@ namespace ResultadosEstudiantes.Clases
 {
     public class Estudiante
     {
-        //hola 
+        
         public string ApellidoNombre { get; set; }
         public long DNI { get; set; }
         public long? Legajo { get; set; }
